74e4eac46855d8049879a0ccc346b49b5f167429
3541 <alex@emobrien.com>
2018-05-25 13:59:53 +1000

    ld

--- src/arch/x86_common/boot.asm
+++ src/arch/x86_common/boot.asm
@@ -1,3 +1,16 @@
 %include "./multiboot_header.asm"
 
+section .bss
+align 16
+stack_bottom:
+	resb 16384
+stack_top:
 
+section .text
+bits 32
+global _start
+_start:
+	mov esp, stack_top
+
+	mov dword [0xb8000], 0x2F4F2F4B
+	hlt
