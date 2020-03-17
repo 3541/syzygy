66cc5907f82d361c3db4916f0edbf615899a6b8f
3541 <alex@emobrien.com>
2020-03-17 14:58:17 +1100

    Implement freeing in the bitmap frame allocator.
    This is very fragile at the moment, and also lacks any kind of free list
    optimization, so allocation always involves scanning for a free bit.
    However, performance issues are really not a concern at this point.

--- src/memory/bitmap_frame_allocator.rs
+++ src/memory/bitmap_frame_allocator.rs
@@ -1,5 +1,5 @@
 use multiboot2::MemoryAreaIter;
-use spin::Mutex;
+use spin::{Mutex, MutexGuard};
 
 use super::{
     next_aligned_addr, prev_aligned_addr, Frame, FrameAllocator, PhysicalAddress, FRAME_SIZE,
@@ -7,7 +7,7 @@ use super::{
 
 const INITIAL_BITMAP_SIZE: usize = 32768;
 // NOTE: temporary static allocation size
-pub static mut BITMAP: [usize; INITIAL_BITMAP_SIZE] = [1; INITIAL_BITMAP_SIZE];
+pub static mut BITMAP: [usize; INITIAL_BITMAP_SIZE] = [0; INITIAL_BITMAP_SIZE];
 pub struct BitmapFrameAllocator {
     bitmap: Mutex<&'static mut [usize]>,
     base: PhysicalAddress,
@@ -22,14 +22,14 @@ impl BitmapFrameAllocator {
         areas: MemoryAreaIter,
         bitmap: &'static mut [usize],
     ) -> BitmapFrameAllocator {
-        let frame_outside_reserved_area = |a: &PhysicalAddress| {
-            !((*a >= kernel_start && *a <= kernel_end)
+        let frame_in_reserved_area = |a: &PhysicalAddress| {
+            (*a >= kernel_start && *a <= kernel_end)
                 || (*a + FRAME_SIZE <= kernel_end && *a + FRAME_SIZE >= kernel_start)
                 || (*a <= kernel_start && *a + FRAME_SIZE >= kernel_end)
                 || (*a >= multiboot_info_start && *a <= multiboot_info_end)
                 || (*a + FRAME_SIZE <= multiboot_info_end
                     && *a + FRAME_SIZE >= multiboot_info_start)
-                || (*a <= multiboot_info_start && *a + FRAME_SIZE >= multiboot_info_end))
+                || (*a <= multiboot_info_start && *a + FRAME_SIZE >= multiboot_info_end)
         };
         let mut ret = BitmapFrameAllocator {
             bitmap: Mutex::new(bitmap),
@@ -43,20 +43,25 @@ impl BitmapFrameAllocator {
                 FRAME_SIZE,
             ),
         };
-        for area in areas.filter(|a| a.size() as usize >= FRAME_SIZE) {
-            let start_address = next_aligned_addr(area.start_address() as usize, FRAME_SIZE);
-            let end_address = prev_aligned_addr(area.end_address() as usize, FRAME_SIZE);
-            for frame_start in (start_address..=end_address)
-                .step_by(FRAME_SIZE)
-                .filter(frame_outside_reserved_area)
-            {
-                if ret.field(frame_start) >= INITIAL_BITMAP_SIZE {
-                    error!("Index too large at frame 0x{:x}", frame_start);
+        {
+            let mut lock = ret.bitmap.lock();
+            for area in areas.filter(|a| a.size() as usize >= FRAME_SIZE) {
+                let start_address = next_aligned_addr(area.start_address() as usize, FRAME_SIZE);
+                let end_address = prev_aligned_addr(area.end_address() as usize, FRAME_SIZE);
+                for frame_start in (start_address..=end_address)
+                    .step_by(FRAME_SIZE)
+                    .filter(frame_in_reserved_area)
+                {
+                    if ret.field(frame_start) >= INITIAL_BITMAP_SIZE {
+                        error!("Index too large at frame 0x{:x}", frame_start);
+                    }
+
+                    ret.set_used(frame_start, &mut lock);
                 }
-                // TODO: this is fucked
-                ret.bitmap.lock()[ret.field(frame_start)] ^=
-                    BitmapFrameAllocator::mask(frame_start);
             }
+
+            // FIXME hack -- set used the original PML4 so it can be freed when we remap the kernel_end
+            ret.set_used(0x125000, &mut lock);
         }
 
         ret
@@ -73,18 +78,18 @@ impl BitmapFrameAllocator {
     fn address(&self, index: usize, bit_index: usize) -> PhysicalAddress {
         index * 8 * core::mem::size_of::<usize>() * FRAME_SIZE + self.base + bit_index * FRAME_SIZE
     }
-}
 
-impl Index<PhysicalAddress> for BitmapFrameAllocator {
-    type Output = FrameState;
+    // FIXME: This is weird. self is immutable because there is an immutable reference to the lock.
+    // Does this actually make sense? Who knows...
+    fn set_used(&self, address: PhysicalAddress, lock: &mut MutexGuard<&'static mut [usize]>) {
+        let field = self.field(address);
+        let mask = Self::mask(address);
 
-    fn index(&self, address: PhysicalAddress) -> &Self::Output {
-        let f = self.bitmap.lock()[self.field(address)];
-        if f & BitmapFrameAllocator::mask(address) == FrameState::FREE as usize {
-            &FrameState::FREE
-        } else {
-            &FrameState::USED
+        if lock[field] & mask == 1 {
+            panic!("Tried to set an already used address as used.");
         }
+
+        lock[field] |= mask;
     }
 }
 
@@ -97,8 +102,9 @@ impl FrameAllocator for BitmapFrameAllocator {
             }
             ret
         }
+
         for (i, field) in (**self.bitmap.lock()).iter_mut().enumerate() {
-            if *field == FrameState::FREE as usize {
+            if *field == 0 {
                 let unset = first_unset_bit(*field);
                 let r_addr = self.address(i, unset);
                 *field |= 1 << unset;
@@ -128,7 +134,16 @@ impl FrameAllocator for BitmapFrameAllocator {
         None
     }
 
-    fn free(&mut self, _frame: Frame) {
-        warn!("Freeing not yet implemented!");
+    fn free(&mut self, frame: Frame) {
+        trace!("Freeing {:x?}", frame);
+
+        let field = self.field(frame.address());
+        let mask = Self::mask(frame.address());
+        let mut bitmap = self.bitmap.lock();
+        if bitmap[field] & mask != 1 {
+            panic!("DOUBLE FREE");
+        }
+
+        bitmap[field] ^= mask;
     }
 }
