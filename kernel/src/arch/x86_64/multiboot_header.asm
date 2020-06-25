c21ae7cd129b0e98953c249c09c42b3436a5466d
3541 <alex@emobrien.com>
2020-06-25 16:56:51 +1000

    Remove cruft

--- kernel/src/arch/x86_64/multiboot_header.asm
+++ kernel/src/arch/x86_64/multiboot_header.asm
@@ -5,22 +5,6 @@ header_start:
 	dd header_end - header_start ; length
 	dd 0x100000000 - (0xE85250D6 + 0 + (header_end - header_start)) ; checksum
 
-;align 8
-    
-;    dw 4    ; flags tag
-;    dw 0    ; flags
-;    dd 12   ; size
-;    dd 1    ; text mode
-
-;align 8
-
-;    dw 5    ; framebuffer tag
-;    dw 0    ; flags
-;    dd 20   ; size
-;    dd 80   ; width
-;    dd 43   ; height
-;    dd 0    ; depth (text mode)
-
 align 8
 
 	; end tag
