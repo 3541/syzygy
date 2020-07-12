89dea70f25cf7f330604bec3d07daa87066d354c
3541 <alex@emobrien.com>
2020-07-12 20:32:08 +1000

    Prevent simultaneous use of TempPage without a panic.

--- kernel/src/memory/paging/temp_page.rs
+++ kernel/src/memory/paging/temp_page.rs
@@ -1,14 +1,23 @@
+use core::sync::atomic::{AtomicBool, Ordering};
+
 use super::mapper::Mapper;
 use super::table::{ActiveTopLevelTable, Table, PML4};
 use super::EntryFlags;
+use crate::arch::pause;
 use crate::memory::{Frame, PhysicalAddress, VirtualAddress};
 
 pub struct TempPage(pub Frame);
+
+static IN_USE: AtomicBool = AtomicBool::new(false);
 
 impl TempPage {
     const ADDRESS: VirtualAddress = unsafe { VirtualAddress::new_const(0xe000e000) };
 
     pub fn map(&mut self, active: &mut ActiveTopLevelTable) {
+        while IN_USE.compare_and_swap(false, true, Ordering::SeqCst) {
+            pause();
+        }
+
         assert!(active.virtual_address_to_frame(Self::ADDRESS).is_none());
         active
             .map_to(Self::ADDRESS, &self.0, EntryFlags::WRITABLE)
@@ -17,6 +26,7 @@ impl TempPage {
 
     pub fn unmap(self, active: &mut Mapper) -> Frame {
         active.unmap(Self::ADDRESS).flush();
+        IN_USE.store(false, Ordering::SeqCst);
         self.0
     }
 
