4b40e41881d9a7130436dba55ce24f1091d349a8
3541 <alex@emobrien.com>
2020-10-08 15:16:58 +1100

    Add mem::VirtualRegion -- Simple interval structure.

--- kernel/src/mem/mod.rs
+++ kernel/src/mem/mod.rs
@@ -1,7 +1,10 @@
 pub mod arch;
 pub mod map;
+mod region;
 mod types;
 
 pub use types::{
     size, Address, PhysicalAddress, RawPhysicalAddress, RawVirtualAddress, VirtualAddress,
 };
+
+pub use region::VirtualRegion;
