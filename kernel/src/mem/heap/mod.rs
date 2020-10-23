244e8da015c8dfbe8c34392407a3e50d93161f3f
3541 <alex@emobrien.com>
2020-10-23 21:09:29 +1100

    Rename LLAlloc to LLAllocator.

--- kernel/src/mem/heap/mod.rs
+++ kernel/src/mem/heap/mod.rs
@@ -6,7 +6,7 @@ use alloc::alloc::{GlobalAlloc, Layout};
 
 use super::size;
 use crate::util::sync::Transform;
-use ll::{LLAlloc, LLNode};
+use ll::{LLAllocator, LLNode};
 
 // Statically allocated initial heap to allow for dynamic allocation before the
 // actual VM system is up. *const () is used here to enforce pointer-size
@@ -14,12 +14,12 @@ use ll::{LLAlloc, LLNode};
 const INIT_HEAP_SIZE: usize = 32 * size::KB;
 static mut INIT_HEAP: [LLNode; size::units_of::<LLNode>(INIT_HEAP_SIZE)] =
     [LLNode::new(INIT_HEAP_SIZE); size::units_of::<LLNode>(INIT_HEAP_SIZE)];
-// The initial heap uses LLAlloc because it is small, making space
+// The initial heap uses LLAllocator because it is small, making space
 // efficiency and defragmentation a higher priority than performance.
-static mut INIT_ALLOCATOR: LLAlloc = unsafe { LLAlloc::from_slice(&mut INIT_HEAP) };
+static mut INIT_ALLOCATOR: LLAllocator = unsafe { LLAllocator::from_slice(&mut INIT_HEAP) };
 
 #[cfg_attr(not(test), global_allocator)]
-static ALLOCATOR: Transform<&'static LLAlloc, &'static LLAlloc> =
+static ALLOCATOR: Transform<&'static LLAllocator, &'static LLAllocator> =
     unsafe { Transform::new(&INIT_ALLOCATOR) };
 
 unsafe impl<T: GlobalAlloc> GlobalAlloc for Transform<T, T> {
