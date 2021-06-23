de62a2cf0852667f585dd0d58f8fb3ce10aa62da
Alex O'Brien <3541ax@gmail.com>
2021-06-23 17:00:10 +1000

    TIDY: Dependency updates.

--- Build/Boot.hs
+++ Build/Boot.hs
@@ -2,8 +2,8 @@ module Build.Boot where
 
 import Control.Exception.Extra
 import Data.Foldable
+import Data.List
 import qualified Data.Map.Strict as Map
-
 import Development.Shake
 import Development.Shake.FilePath
 import Development.Shake.Util
@@ -21,27 +21,30 @@ prefixedMakefileDeps prefix makefile =
 buildBootloader :: String -> Rules ()
 buildBootloader limineDir = do
   -- Use gcc as a proxy dependency for the whole toolchain.
-  let limineCC = limineDir </> "toolchain" </> "bin" </> "i386-elf-gcc"
+  let limineCC = limineDir </> "toolchain" </> "bin" </> "x86_64-elf-gcc"
   options <- getShakeOptionsRules
   let threads = shakeThreads options
+  let limineHddBin = limineDir </> "bin" </> "limine-hdd.bin"
 
-  limineDir </> "bin" </> "limine-hdd.bin" %> \_out -> do
-    need [
-      limineCC,
-      limineDir </> "Makefile",
-      limineDir </> "decompressor" </> "Makefile",
-      limineDir </> "stage23" </> "Makefile",
-      limineDir </> "stivale" </> "stivale2.h"
+  limineHddBin %> \_out -> do
+    need
+      [ limineCC,
+        limineDir </> "Makefile",
+        limineDir </> "decompressor" </> "Makefile",
+        limineDir </> "stage23" </> "Makefile",
+        limineDir </> "stivale" </> "stivale2.h"
       ]
 
     cmd_ "make -j" (show threads) "-C" limineDir "limine-bios"
     decompressorDepFiles <- getDirectoryFiles limineDir ["decompressor//*.d"]
     stage2DepFiles <- getDirectoryFiles limineDir ["stage23//*.d"]
-    forM_ (map (limineDir </>) decompressorDepFiles) (prefixedMakefileDeps $ limineDir </> "decompressor")
-    forM_ (map (limineDir </>)  stage2DepFiles) (prefixedMakefileDeps $ limineDir </> "stage23")
+    forM_
+      (map (limineDir </>) decompressorDepFiles)
+      (prefixedMakefileDeps $ limineDir </> "decompressor")
+    forM_ (map (limineDir </>) stage2DepFiles) (prefixedMakefileDeps $ limineDir </> "stage23")
 
   limineDir </> "bin" </> "limine.sys" %> \_out -> do
-    need [limineDir </> "bin" </> "limine-hdd.bin"]
+    need [limineHddBin]
 
   limineDir </> "stivale" </> "stivale2.h" %> \_out -> do
     need [limineDir </> "Makefile"]
@@ -49,19 +52,25 @@ buildBootloader limineDir = do
     cmd_ "make" "-C" limineDir "stivale"
 
   limineDir </> "limine-install" </> "limine-hdd.o" %> \_out -> do
-    need [limineDir </> "bin" </> "limine-hdd.bin"]
-    cmd_ "make" "-C" (limineDir </> "limine-install")
-      ("LIMINE_HDD_BIN=" ++ (".." </> "bin" </> "limine-hdd.bin")) "limine-hdd.o"
+    need [limineHddBin]
+    cmd_
+      "make"
+      "-C"
+      (limineDir </> "limine-install")
+      ("LIMINE_HDD_BIN=" ++ (".." </> "bin" </> "limine-hdd.bin"))
+      "limine-hdd.o"
 
   limineDir </> "bin" </> "limine-install" %> \_out -> do
-    need [limineDir </> "limine-install" </> "Makefile"]
+    need [limineDir </> "limine-install" </> "Makefile", limineHddBin]
     makefile <- liftIO (readFile $ limineDir </> "limine-install" </> "Makefile")
-    let makefileDeps = map ((limineDir </> "limine-install") </>) $
-                       Map.fromListWith (++) (parseMakefile makefile) Map.! "limine-install"
+    let makefileDeps =
+          filter (notElem '$') $
+            map ((limineDir </> "limine-install") </>) $
+              Map.fromListWith (++) (parseMakefile makefile) Map.! "limine-install"
     need makefileDeps
 
     cmd_ "make -j" (show threads) "-C" limineDir ("bin" </> "limine-install")
 
   limineCC %> \_out -> do
-    need [limineDir </> "scripts" </> "make_toolchain_bios.sh"]
-    cmd_ (Cwd $ limineDir) ("scripts" </> "make_toolchain_bios.sh") "toolchain" ("-j" ++ show threads)
+    need [limineDir </> "scripts" </> "make_toolchain.sh"]
+    cmd_ (Cwd limineDir) ("scripts" </> "make_toolchain.sh") "toolchain" ("-j" ++ show threads)
